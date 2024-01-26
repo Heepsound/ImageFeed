8ca2f931d28dbdbc3d2796d22d48c33f72c85a40
Heepsound <heepsound@yandex.ru>
2024-01-26 16:36:22 +0300

    Добавлены ProfileViewController и TabBarController

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    static let reuseIdentifier = "ImageListCell"
-    
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    static let reuseIdentifier = "ImageListCell"
 }
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Владимир Горбачев on 25.01.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private weak var userPhotoImageView: UIImageView!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var exitButton: UIButton!
+    
+    @IBAction private func touchUpInsideExitButton() {
+        
+    }
+}
+
